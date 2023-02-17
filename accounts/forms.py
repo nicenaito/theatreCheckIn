e94db2278e0327727dd6da4848245a4037ac089a
nicenaito <nicenaito@gmail.com>
2023-02-17 09:50:44 +0900

    サインアップ画面を作成。

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,20 @@
 from django.contrib.auth.forms import AuthenticationForm 
-
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.models import User
 
 class LoginForm(AuthenticationForm):
-    """ログオンフォーム"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
             field.widget.attrs['placeholder'] = field.label  
+            
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import(LoginView, LogoutView)
 from .forms import LoginForm
+from .forms import SignupForm
 
 
 class Login(LoginView):
@@ -14,3 +14,17 @@ class Login(LoginView):
 class Logout(LoginRequiredMixin, LogoutView):
     """ログアウトページ"""
     template_name = 'accounts/login.html'
+
+def signup(request):
+    if request.method == 'POST':
+
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = SignupForm()
+   
+    param = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', param)
