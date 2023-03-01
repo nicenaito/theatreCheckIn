31b4cec80e8080207d966cb13d9ed47a5c2f0e31
fukunaga <nicenaito@gmail.com>
2023-03-01 09:34:15 +0900

    ログ出力を追加。

--- theatreCheckIn/management/commands/collectMovie.py
+++ theatreCheckIn/management/commands/collectMovie.py
@@ -3,13 +3,18 @@ from theatreCheckIn.models import Movies
 from django.conf import settings
 import requests
 from django.utils import timezone
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        logger.info("collectMovie start")
         url = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + settings.API_MOVIE + "&language=ja&region=jp"
         total_pages = self.getNumberOfResults(url)
         
         if total_pages > 0:
+            logger.info("Total page of the response: " + str(total_pages))
             movies_dict = []
             movies_dict = self.makeNowPlayingMoviesDict(url+"&page=", total_pages, movies_dict)
             
@@ -19,8 +24,8 @@ class Command(BaseCommand):
                 for data in movie_list["results"]:
                     now_playing_movie_id_list.append(data["id"])
                     try:
-                        Movies.objects.get(pk=data["id"])
-                        # print("Not saved!")
+                        movie = Movies.objects.get(pk=data["id"])
+                        logger.info("The movie has been on DB. Title: " + movie.movie_title)
                         
                     except (KeyError, Movies.DoesNotExist):
                         register_movie = Movies(
@@ -33,7 +38,7 @@ class Command(BaseCommand):
                         overview=data["overview"],
                         )
                         register_movie.save()
-                        print("Saved!")
+                        logger.info("The movie has been registered in DB. Title: " + data["title"])
             
             registered_movies = Movies.objects.values_list('movie_id', flat=True)
             
@@ -41,25 +46,33 @@ class Command(BaseCommand):
             for query_result in registered_movies:
                 if query_result not in now_playing_movie_id_list:
                     selected_movie = Movies.objects.get(pk=query_result)
-                    if selected_movie <= timezone.now():
+                    if selected_movie.now_playing == True and selected_movie.pub_date < timezone.now().date():
                         selected_movie.now_playing = False
                         selected_movie.updated_datetime = timezone.now()
                         selected_movie.save()
-                        print("Saved!")
+                        logger.info("The flag of 'now playing' for the movie has been updated to False. Title: " + selected_movie.movie_title)
+                        
+        logger.info("collectMovie end")
                     
     def getNumberOfResults(self, base_url):
+        logger.info("getNumberOfResults start")
         payload = {}
         headers = {}
 
+        logger.info("API Request")
         response = requests.request("GET", base_url, headers=headers, data=payload)
+        logger.info("API Response recieved")
 
         json_dict = response.json()
         
         total_pages = json_dict["total_pages"]
         
+        logger.info("getNumberOfResults end")
+        
         return total_pages
     
     def makeNowPlayingMoviesDict(self, base_url, number_of_results, movies_dict):
+        logger.info("makeNowPlayingMoviesDict start")
         i = 1
         while i <= number_of_results:
         # for i in range(1, number_of_results):
@@ -67,10 +80,15 @@ class Command(BaseCommand):
             payload = {}
             headers = {}
 
+            logger.info("API Request")
             response = requests.request("GET", url, headers=headers, data=payload)
+            logger.debug("Response: " + str(response.json()))
+            logger.info("API Response recieved")
 
             movies_dict.append(response.json())
+            logger.debug("Dictionary is made: " + str(movies_dict))
             
             i += 1
             	
+        logger.info("makeNowPlayingMoviesDict end")
         return movies_dict
